f295a757b6a28cbd96ceea2853e12f1436aea019
NoShadiah <nabawangashadiah20@gmail.com>
2023-05-04 13:11:35 +0300

    working on datatypes in java

---
+++ src/Datatypes.java
@@ -0,0 +1,14 @@
+public class Datatypes{
+    public static void main(String[] args){
+//These are basic variables with different datatype, for some of them am adding the maximum variable
+        byte my_byte = 127;
+        short my_short = 32767;
+        int my_int = 2_147_483_647;
+        long my_long = 2_000_000_354L;
+
+    }
+
+    public static int addition(int x, int y){
+        int result =
+    }
+}
