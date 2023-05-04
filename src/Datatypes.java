122a1254a0952a64f9c499f2820b7647ca1feedd
NoShadiah <nabawangashadiah20@gmail.com>
2023-05-04 13:17:27 +0300

    working on datatypes in java

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -5,10 +5,15 @@ public class Datatypes{
         short my_short = 32767;
         int my_int = 2_147_483_647;
         long my_long = 2_000_000_354L;
-
+        int my_additionFunction = addition(my_short, my_int);
+        int resultant = addition(2, 5);
+        System.out.println(my_additionFunction);
+        System.out.println(resultant);
     }
 
     public static int addition(int x, int y){
-        int result =
+        int result = x + y;
+
+        return(result);
     }
 }
