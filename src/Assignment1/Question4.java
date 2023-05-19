53a2bfcd6fb8b64bdc8de8ddee24989a56fb8047
NoShadiah <nabawangashadiah20@gmail.com>
2023-05-19 14:09:31 +0300

    Assignment1, qusetion4

--- src/Assignment1/Question4.java
+++ src/Assignment1/Question4.java
@@ -1,4 +1,15 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class Question4 {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the number you want me to count:");
+
+        String userInput = myObj.nextLine();  // Read user input
+        int Counted_input = userInput.length();
+        System.out.println("The number of digits in  " + userInput + " is " + Counted_input);
+
+    }
 }
