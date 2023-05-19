d62e0999b60a0be78ffb72fa9fa8f13a8d33cb7e
NoShadiah <nabawangashadiah20@gmail.com>
2023-05-19 13:52:28 +0300

    Assignment1, qusetion3

--- src/Assignment1/Question3.java
+++ src/Assignment1/Question3.java
@@ -1,5 +1,22 @@
 package Assignment1;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+//3.	Write a Java program to find the factorial of a number.
 public class Question3 {
+    public static void main(String[] args){
+        int mynumber = 4;
+        int myFactorial=1;
+        int[] Array_in_range = IntStream.rangeClosed(1, 4).toArray();
+        for (int i = 0; i<Array_in_range.length; i++){
+//            Re-assigning the factorial variable by multiplying each element
+//            in the array with the product's reassigned value
+            myFactorial = Array_in_range[i]*myFactorial;
+        }
+        System.out.println("The factorial of "+ mynumber +" is "+ myFactorial );
+    }
 
 }
